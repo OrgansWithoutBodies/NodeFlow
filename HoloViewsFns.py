a94443d5fc8d619b94b840cd895413cbc837ee8e
OrgansWithoutBodies <vsliupas@gmail.com>
2018-11-02 19:53:12 -0700

    first pushd to GH

--- HoloViewsFns.py
+++ HoloViewsFns.py
@@ -8,10 +8,11 @@ DONE:
             default data connected to right sql keys
 FUTURE GOALS
     ~0.3:   make weeks connect more easily
-    ~0.5:   Flexible SQL keys
+    ~0.5:   Flexible SQL keys, connectable w multiple dbs at once
             get datashader working flexibly
             check datetype?
-    1.0:    get nodes implemented right
+            datetimes are well-defined w different gaps    
+    1.0:    get NodeFlow nodes implemented right
 default layouts - ():[]:{}:||
     ~#NAME#Graphtype(x,y,...):[sliders]:{overlay}:|Adjoints| fn $VAR$ --comment~
     Donations:
@@ -20,9 +21,23 @@ default layouts - ():[]:{}:||
         Curve/{Stacked}Bar(day of week,amount):{Item}-$APW$ --difference from average
         Curve():|Spike(time of day,donations)|
         #CATMAPSANKEY#Sankey(Catmap->DonationCategory,Amount) --can't include nodes w 0, initial node name must be different from terminal node name
-
+            Sankey between CATMAPSANKEY and Transaction sum? - other catmap
     Transactions:
         Scatter():[]:{}
+        Hist/curve/**(day's total earned,day) --Superimposable line of goal per transaction effects other graphs?
+        #AVE({{{Price}--per--{product purchased}}--per--{day}})
+        #AVE({%({discounts}--per--{transaction}})
+    
+        #most popular category
+        #ave prc per item per category
+        #loop around/modulo day of week/hour of day
+        #categories per transaction
+        #discounts per trans per day
+        #Ave discount $/% per trans per day
+
+    Schedule:
+        #Density
+        #Teams
         
     """
 import holoviews as hv
@@ -37,6 +52,15 @@ from bokeh.models import HoverTool
 from holoviews.operation.datashader import datashade
 
 from Objects import Node
+
+
+ddir=r'/home/v/Projects/Opphouse/Data'
+dpath=ddir+r'/donations.db'
+tpath=ddir+r'/transactions.db'
+spath=ddir+r'/schedules.db'
+ppath=ddir+r'/products.db'
+graphpath=r'/home/v/Projects/OHSite/Website\opphouse\webapp\graphs'
+
 #Stacked Curves -  hv.Area.stack(overlay) http://holoviews.org/reference/elements/matplotlib/Area.html
 #base tutorial - http://pyviz.org/tutorial/
 #datashader - http://datashader.org/getting_started/2_Pipeline.html
@@ -44,49 +68,44 @@ from Objects import Node
 #Spike Train  -  http://holoviews.org/user_guide/Customizing_Plots.html
 #dashboard info - http://dev.holoviews.org/user_guide/Dashboards.html
 #overlay&Holomap - http://holoviews.org/user_guide/Building_Composite_Objects.html
-
+#bokeh dashboard https://towardsdatascience.com/data-visualization-with-bokeh-in-python-part-iii-a-complete-dashboard-dc6a86aa6e23
 #@todo able to choose between holomap (long wait upfront w big data) or dynamicmap (longish wait during )
-def slicedata(fdict=None,overlaydims=None):#gets relevant slice of data, used to figure out which plots get shown for multidimensional graphs
-#    for 
-        flist=list(fdict.keys())
-    #    print(flist)
-        truth=1
-        for i in range(len(flist)):
-            truth=(truth) & (d[flist[i]]==fdict[flist[i]])
-        sdata=d[truth]
-    #    print(len(sdata))
-        return graphtype(sdata,basedims[0],basedims[1])#return limited sdata and use basedims
-
-def fs(label=None):#f's function - gets all the individual values to iterate through for dimensions which will be sliders
-        return d[label].unique()
-def timerange(NInit,NLast,NStep):
-    ts=dict()
-    for n in range(NInit,NLast,NStep):
-        ts[n]=timegraph(n,['date','item'],['donor','amt'],['amt'],graphtype=hv.Scatter)
-    return ts
-def timegraph(N,basedims,sliderdims,overlaydims=None,tooltipdims=None,**kw):
-    t=Timer()
-    makehv(basedims=basedims,sliderdims=sliderdims,overlaydims=overlaydims,tooltipdims=tooltipdims,N=N,**kw)
-    return(t.lap())
+def getfft(xinp,yinp):
     
+    n=len(xinp)//2
+    xinp,yinp=xinp[:n],1/(n)*np.abs(FFT.fft(np.abs(FFT.fft(yinp))))
+            
     
-    
-    
-def defaultgraphs():
+def defaultgraphs(typ='don'):
     #@todo have way of getting all in same instance 
     #@todo improve loadtime - 16 secs on my laptop
-    d=getdata(-1,True)
-    tbl=hv.Table(getweekday(d))
-    
-    HVServer(tbl.to.bars(['weekday','item'],['value']))#@todo have better way of doing this
-    makehv(data=weeklyitembrkdwn(d),basedims=['week','value'],sliderdims=['item'],overlaydims=['item'],server=True,graphtype=hv.Curve) #sum of item amts per week
-    HVServer(catmapsankey(perc=True))
+    if typ=='don':
+        d=getdata(-1,True)
+        tbl=hv.Table(getweekday(d))
+        
+        HVServer(tbl.to.bars(['weekday','item'],['value']))#@todo have better way of doing this
+    #    
+    #    makehv(data=getweekday(d),basedims=['weekday','item'],sliderdims=['value'],graphtype=hv.Bars,server=True)#@todo have better way of doing this
+    #    HVServer(d.to.bars(['weekday','item'],['value']))#@to
+    #    makehv(data=getweekday(d),basedims=['weekday','item'],sliderdims=['value'],overlaydims=['value'],graphtype=hv.Bars,server=True)#@todo have better way of doing this
+        makehv(data=weeklyitembrkdwn(d),basedims=['week','value'],sliderdims=['item'],overlaydims=['item'],server=True,graphtype=hv.Curve) #sum of item amts per week
+        HVServer(catmapsankey(perc=True))
+    else:
+        d=gettransdata()
+        makehv(d,['Itemdesc','Quantity'],['Customer','Discount Name'],['Customer'],server=True,graphtype=hv.Points)
 def catmapsankey(N=-1,perc=False):
     d=getdata(N,mapcats=False)      
     tperc=sum(d['amt']) if perc else 1#if perc then ends up dividing edge values by total else divides by 1
-    map=sql.connect(path).execute("SELECT categorymap.name,donationcategories.name FROM CATEGORYMAP JOIN DONATIONCATEGORIES ON CATEGORYMAP.MAPSTOID=DONATIONCATEGORIES.ID").fetchall()
+    map=sql.connect(dpath).execute("SELECT categorymap.name,donationcategories.name FROM CATEGORYMAP JOIN DONATIONCATEGORIES ON CATEGORYMAP.MAPSTOID=DONATIONCATEGORIES.ID").fetchall()
     mm=[['Original Category: '+m[0],'New Category: '+m[1],sum(d['amt'][d['item']==m[0]])/tperc]for m in map if sum(d['amt'][d['item']==m[0]])>0 ]
     return hv.Sankey(mm)
+def xwithmosty(data,xkey,ykey):#returns the value of data at xkey associated with the most ykeys    
+    pass
+def difffrommean(data,meandim):#difference of each row from mean value of each column
+    d=data.groupby(meandim)
+    meanfn= lambda x: (x - x.mean())
+    
+    return d.transform(meanfn)
 def weeklyitembrkdwn(d=None):
     if d is None:
         d=getdata(-1,True)
@@ -94,6 +113,16 @@ def weeklyitembrkdwn(d=None):
     t.rename(columns={'date':'week'},inplace=True)
 #    makehv(data=weeklyitembrkdwn(),basedims=['date','value'],sliderdims=['item'],overlaydims=['item'],server=True,graphtype=hv.Curve)
     return t
+#@todo for timeseries have dropdown for resampling https://stackoverflow.com/questions/17001389/pandas-resample-documentation#17001474
+    
+reskeys={'M':"Monthly",
+         'D':"Daily",
+         'W':"Weekly (SUN)",
+         'W-MON':"Weekly (MON)",
+         'Q':"Quarterly",
+         "H":"Hourly",
+         "T":"Minutely",
+         "S":"Second-by-Second"}
 def getweekday(d,dkey='date',vkey='amt',itmkey='item'):#@todo have simple way of deciding startday
     #@todo some way of easily doing weighted sum
     d[dkey]=pd.to_datetime(d[dkey])
@@ -101,13 +130,16 @@ def getweekday(d,dkey='date',vkey='amt',itmkey='item'):#@todo have simple way of
     ds=d.groupby(['weekday',itmkey]).sum()
     md=meltweek(ds[vkey].unstack(1),'weekday')
     return md
-
+def linreg(x,y):
+    return sp.stats.stats.linregress(x, y)
    #@todo some way to mark date column
+def groupbydatetime(d,dkey,kkey,vkey,n):#@todo splits by n days
+    pass
    #dkey - column name of date, kkey - what to group by, vkey - column to weigh kkey, startkey - 'W','W-SUN','W-MON' - how week starts, fn - how group is aggregated
 def groupbyweek(d,dkey='date',kkey='item',vkey='amt',startkey='W-SUN',fn=sum,tidy=False):#fits better somewhere else?
     d[dkey]=pd.to_datetime(d[dkey])
     d.groupby([pd.Grouper(key=dkey,freq=startkey)])
-#    
+#    -
 #    if  pd.core.dtypes.dtypes.DatetimeTZDtype not in d.dtypes.values:#checks which values are datetimes
 #        d[dkey]=pd.to_datetime(d[dkey])
 #        print(d)
@@ -125,7 +157,7 @@ def groupbyweek(d,dkey='date',kkey='item',vkey='amt',startkey='W-SUN',fn=sum,tid
         return meltweek(td[vkey])
     else:
         return td
-def meltweek(d,dkey='date'):
+def meltweek(d,dkey='date'):#tidies wide data
     if dkey not in d.columns:
         d[dkey]=d.index
     m=pd.melt(d,dkey)
@@ -133,6 +165,61 @@ def meltweek(d,dkey='date'):
         m[dkey]=m[dkey].dt.strftime('%x')
     return m
 
+
+sqlstrs={tpath:{'DBColumns':{'itemdesc':'Itemdesc',
+                             'Barcode':'Barcode',
+                             'Quantity':'Item Quantity',
+                             'DiscountAmt':'Discount Amount',
+                             'Discounts.Name':'Discount Name',
+                             'Customers.Name':'Customer',
+                             'Purchases.timestamp':'timestamp',
+                             'Employees.Name':'Cashier'},
+                    'From':'PurchaseLines',
+                    'Joins':{'Purchases':'Purchases.ID=PurchaseLines.PurchaseID',
+                             'Customers':'Customers.ID=Purchases.CustomerID',
+                             'Employees':'Employees.ID=Purchases.CashierID',
+                             'Discounts' :'Discounts.ID=PurchaseLines.DiscountID'}
+                },# 'categorymap.name'/'donationcategories.name':'item',#@todo have a way to include this choice
+        dpath:{'DBColumns':{'donationid':'donation',
+                 'quantity':'amt',
+                 'timestamp':'date',
+                 'Discounts.Name':'donor',
+                 'Customers.Name':'Customer',
+                 'Purchases.timestamp':'timestamp',
+                 'Employees.Name':'Cashier'},
+        'From':'DonationLines',
+        'Joins':{'Donors':'Donors.ID=Donations.DonorID',
+                 'Donations':'Donations.ID=DonationLines.DonationID',
+                 'CategoryMap':'CategoryMap.ID=DonationLines.CategoryID',
+                 'DonationCategories':'CategoryMap.MapsToID=DonationCategories.ID'}
+    }
+}# FROM DONATIONLINES 
+#                            JOIN DONORS ON DONATIONS.DONORID=DONORS.ID 
+#                            JOIN DONATIONS ON DONATIONLINES.DONATIONID=DONATIONS.ID
+#                            JOIN CATEGORYMAP ON DONATIONLINES.CATEGORYID=CATEGORYMAP.ID""".format(maps[mapcats][0],nameformat)
+#    if mapcats:
+def getsqldata(pathname,tbldict,N=-1,dmapcats=True,nameformat="lastname||', '||firstname"):
+    tbldict.keys()
+    sqlquery="SELECT {0} FROM {1} JOIN {2}".format(", ".join( tbldict['DBColumns'].keys()),tbldict['From']," JOIN ".join([" ON ".join([t,tbldict['Joins'][t]]) for t in tbldict['Joins'].keys()]))
+    c=sql.connect(tpath)
+    print(sqlquery)
+    data=c.execute(sqlquery).fetchall()
+    c.close()
+    d=pd.DataFrame(data[0:N],columns=list(tbldict['DBColumns'])) #handle base data
+    
+    return d
+def gettransdata():#@todo joins with item categories right
+    
+     sqlquery="""SELECT itemdesc,Barcode,Quantity,DiscountAmt,Customers.Name, Purchases.timestamp, Discounts.Name, Employees.Name
+                    FROM PurchaseLines 
+                        JOIN Purchases ON Purchases.ID=PurchaseLines.PurchaseID
+                        JOIN Customers ON Customers.ID=Purchases.CustomerID
+                        JOIN Employees ON Employees.ID=Purchases.CashierID 
+                        JOIN Discounts ON Discounts.ID=PurchaseLines.DiscountID"""
+     c=sql.connect(tpath)
+     data=pd.DataFrame(c.execute(sqlquery).fetchall(),columns=['Itemdesc','Barcode','Quantity','Discount Amount','Customer','Timestamp','Discount Name','Cashier'])
+     c.close()
+     return data
 def getdata(N=-1,mapcats=True,nameformat="lastname||', '||firstname"):
     maps={False:['categorymap.name'," JOIN CATEGORYMAP ON DONATIONLINES.CATEGORYID=CATEGORYMAP.ID"],#@todo make less janky
           True:['donationcategories.name',' JOIN DONATIONCATEGORIES ON CATEGORYMAP.MAPSTOID=DONATIONCATEGORIES.ID']}
@@ -144,15 +231,15 @@ def getdata(N=-1,mapcats=True,nameformat="lastname||', '||firstname"):
                             JOIN CATEGORYMAP ON DONATIONLINES.CATEGORYID=CATEGORYMAP.ID""".format(maps[mapcats][0],nameformat)
     if mapcats:
         sqlquery=sqlquery+""" JOIN DONATIONCATEGORIES ON CATEGORYMAP.MAPSTOID=DONATIONCATEGORIES.ID"""
-    data=sql.connect(path).execute(sqlquery).fetchall()
+    c=sql.connect(dpath)
+    data=c.execute(sqlquery).fetchall()
+    c.close()
 #    print(len(data))
 #    [0:N]
     
     d=pd.DataFrame(data[0:N],columns=['donation','item','amt','date','donor']) #handle base data
     return d
-path=r'C:\Users\V\Python\Opphouse\Data\donations.db'
-graphpath=r'C:\Users\V\Python\Opphouse\Projects\Website\opphouse\webapp\graphs'
-def makehv(basedims,data=None,sliderdims=None,overlaydims=None,histdims=None,tooltipdims=None,dshade=False,graphtype=hv.Scatter,rendertype='bokeh',N=-1,server=False,adj=None,sz=(1000,500)):
+def makehv(data=None,basedims=None,sliderdims=None,overlaydims=None,histdims=None,tooltipdims=None,dshade=False,graphtype=hv.Scatter,rendertype='bokeh',N=-1,server=False,adj=None,sz=(1000,500)):
     #@todo have adjoin choices clear
     hv.extension(rendertype)
     #@todo optional if maps to base categories or categorymap
@@ -248,11 +335,10 @@ def HVServer(hvobj,rendertype='bokeh',instance=None):
     s.start()
     s.show('/')
 
-
 #HV NODES HERE 
 #@todo optional background grid
 #
-class HVAdjoinNode(Node):
+class HVAdjoinNode(Node):#
     pass
 class HVGraphNode(Node):
     #inputs:
